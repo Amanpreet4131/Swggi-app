8b5ef4b4348b59961bb84972a5b4e84fc218151f
Amanpreet4131 <ap946186@gmail.com>
2024-08-13 07:51:43 +0530

    new_chages

--- swggi-clone/src/App.jsx
+++ swggi-clone/src/App.jsx
@@ -7,7 +7,9 @@ function App() {
     <>
     <Header/>
     <Catagory/>
-    <Toprest></Toprest>
+    <Toprest/>
+    
+    
 
     </>
   );
--- swggi-clone/src/componet/toprest.jsx
+++ swggi-clone/src/componet/toprest.jsx
@@ -1,37 +1,49 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { GoArrowRight, GoArrowLeft } from "react-icons/go";
+import Card from "./Card";
 
 export default function Toprest() {
+  const [data, setdata] = useState([]);
 
-    const[data, setdata] = useState([]);
+  const fetchTopRestaurant = async () => {
+    const response = await fetch("http://localhost:5000/top-restaurant-chains");
+    const apiData = await response.json();
+    setdata(apiData);
+  };
 
-    const fetchTopRestaurant = async () => {
-        const response = await fetch("http://localhost:5000/top-restaurant-chains")
-        const apiData = await response.json();
-        setdata(apiData);
-    }
-
-    useEffect(
-        () => {
-            fetchTopRestaurant();
-        }
-    )
+  useEffect(() => {
+    fetchTopRestaurant();
+  });
   return (
     <>
-    <div className="max-w-[1500px] mx-auto ">
-    <div className="flex items-center justify-between">
-          <div className="text-[25px] items-bold">Top restaurant chains in Chandigarh</div>
+      <div className="max-w-[1500px] mx-auto ">
+        <div className="flex items-center justify-between">
+          <div className="text-[25px] items-bold">
+            Top restaurant chains in Chandigarh
+          </div>
           <div className="flex ">
             <div className=" flex justify-center items-center  w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2">
-              <GoArrowLeft/>
+              <GoArrowLeft />
             </div>
             <div className=" flex justify-center items-center  w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2">
-              <GoArrowRight  />
+              <GoArrowRight />
             </div>
           </div>
-        
         </div>
-    </div>
+        <div className="flex gap-5 overflow-hidden">
+
+          {
+            data.map(
+              (d,index) => {
+                return <Card {...d} key={index}/>
+
+              }
+            )
+          }
+          
+         
+        </div>
+      </div>
     </>
-  )
+  );
 }
